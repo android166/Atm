c53bd936e1df5f74dfe9fa287d7d5c131461a098
Hank Tom <hanktom@gmail.com>
2016-04-25 21:50:04 +0800

    返回時不帶結果

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -3,16 +3,35 @@ package com.tom.atm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edUserid;
+    private EditText edPasswd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViews();
+
+
+    }
+
+    private void findViews() {
+        edUserid = (EditText) findViewById(R.id.userid);
+        edPasswd = (EditText) findViewById(R.id.passwd);
     }
 
     public void login(View v){
+        String userid = edUserid.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if (userid.equals("jack") && passwd.equals("1234")){
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            finish();
+        }
 
     }
 }
