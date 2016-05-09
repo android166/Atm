ed68b211ed1fe94a22c619585d6055dc00539911
Hank Tom <hanktom@gmail.com>
2016-05-09 19:57:44 +0800

    SQLite準備活動

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -117,13 +117,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case R.drawable.func_news:
                 break;
             case R.drawable.func_finance:
+                startActivity(new Intent(this, FinanceActivity.class));
                 break;
             case R.drawable.func_exit:
                 finish();
                 break;
-
         }
-
 
         /*
         int viewId = parent.getId();
