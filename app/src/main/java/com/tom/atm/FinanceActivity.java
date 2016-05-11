d2cbd29180d55ba937a7ae42e560ffe5d57951a5
Hank Tom <hanktom@gmail.com>
2016-05-11 20:18:00 +0800

    兩欄消費記錄清單

--- app/src/main/java/com/tom/atm/FinanceActivity.java
+++ app/src/main/java/com/tom/atm/FinanceActivity.java
@@ -1,19 +1,29 @@
 package com.tom.atm;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
+
+    private ListView list;
+    private MyDBHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
+        helper = new MyDBHelper(this, "expense.db", null, 1);
+        list = (ListView) findViewById(R.id.list);
+        setupListView();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +36,19 @@ public class FinanceActivity extends AppCompatActivity {
                 startActivity(new Intent(FinanceActivity.this, AddActivity.class));
             }
         });
+    }
+
+    private void setupListView() {
+        Cursor c = helper.getReadableDatabase().query("exp",
+                null, null, null, null, null, null);
+        String[] from = {"info", "amount"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,
+                c,
+                from,
+                to);
+        list.setAdapter(adapter);
     }
 
 }
