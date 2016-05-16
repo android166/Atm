1b813460952de08f79ee6aac42daa70c39ab3c6b
Hank Tom <hanktom@gmail.com>
2016-05-16 19:42:15 +0800

    新增消費-今天日期

--- app/src/main/java/com/tom/atm/AddActivity.java
+++ app/src/main/java/com/tom/atm/AddActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
@@ -23,6 +25,10 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         findViews();
         Calendar now = Calendar.getInstance();
+        String today = now.get(Calendar.YEAR)+"-" +
+                (now.get(Calendar.MONTH)+1) + "-" +
+                now.get(Calendar.DAY_OF_MONTH);
+        edDate.setText(today);
         helper = MyDBHelper.getInstance(this);
     }
 
@@ -36,14 +42,6 @@ public class AddActivity extends AppCompatActivity {
         String cdate = edDate.getText().toString();
         String info = edInfo.getText().toString();
         int amount = Integer.parseInt(edAmount.getText().toString());
-        ContentValues values = new ContentValues();
-        values.put("cdate", cdate);
-        values.put("info", info);
-        values.put("amount", amount);
-        long id = helper.getWritableDatabase().insert("exp", null, values);
-        Log.d("AddActivity", id+"");
-        if (id!=-1){
-            Toast.makeText(this, "新增成功", Toast.LENGTH_LONG).show();
-        }
+        ExpenseDao.insert(new Expense(cdate, info, amount));
     }
 }
