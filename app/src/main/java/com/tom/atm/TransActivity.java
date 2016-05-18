9e699196c3d7adb354139cbab44f91d8ec538661
Hank Tom <hanktom@gmail.com>
2016-05-18 21:41:34 +0800

    解析JSON資料(json.org)

--- app/src/main/java/com/tom/atm/TransActivity.java
+++ app/src/main/java/com/tom/atm/TransActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -51,7 +55,19 @@ public class TransActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-
+            try {
+                JSONArray array = new JSONArray(s);
+                for (int i=0; i<array.length(); i++){
+                    JSONObject obj = array.getJSONObject(i);
+                    String account = obj.getString("account");
+                    String date = obj.getString("date");
+                    int amount = obj.getInt("amount");
+                    int type = obj.getInt("type");
+                    Log.d("OBJ", account+"/"+date+"/"+amount+"/"+type);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
