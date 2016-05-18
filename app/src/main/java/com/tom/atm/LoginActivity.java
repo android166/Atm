2c9abe18384406a60075c5c20cf021a29b1e7512
Hank Tom <hanktom@gmail.com>
2016-05-18 20:33:47 +0800

    Atm-網路登入與ProgressBar

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.tom.atm;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +10,13 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edPasswd;
     private CheckBox cbUserid;
     private boolean rememberUserid;
+    private String userid;
+    private String passwd;
+    private ProgressBar pbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +58,15 @@ public class LoginActivity extends AppCompatActivity {
         edUserid = (EditText) findViewById(R.id.userid);
         edPasswd = (EditText) findViewById(R.id.passwd);
         cbUserid = (CheckBox) findViewById(R.id.remember_userid);
+        pbar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     public void login(View v){
-        String userid = edUserid.getText().toString();
-        String passwd = edPasswd.getText().toString();
+        userid = edUserid.getText().toString();
+        passwd = edPasswd.getText().toString();
+        String url = "http://atm201605.appspot.com/login?uid="+ userid +"&pw="+ passwd;
+        new LoginTask().execute(url);
+        /*
         if (userid.equals("jack") && passwd.equals("1234")){
             if (rememberUserid) {
                 getSharedPreferences("atm", MODE_PRIVATE)
@@ -67,6 +82,58 @@ public class LoginActivity extends AppCompatActivity {
 
             finish();
         }
+        */
 
+    }
+
+    class LoginTask extends AsyncTask<String, Void, Integer>{
+        @Override
+        protected void onPreExecute() {
+
+            pbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            int data = 0;
+            try {
+                URL url = new URL(params[0]);
+                InputStream is = url.openStream();
+                data = is.read();
+                Thread.sleep(2000);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(Integer data) {
+            pbar.setVisibility(View.GONE);
+            if (data == 49){
+                if (rememberUserid) {
+                    getSharedPreferences("atm", MODE_PRIVATE)
+                            .edit()
+                            .putString("PREF_USERID", userid)
+                            .commit();
+                }
+                Toast.makeText(LoginActivity.this, "登入成功", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.putExtra("USERID", userid);
+                intent.putExtra("PASSWD", passwd);
+                setResult(RESULT_OK, intent);
+
+                finish();
+            }else{
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setMessage("登入失敗")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        }
     }
 }
